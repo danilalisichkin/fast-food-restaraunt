0307ac43b067ef3aacf4e49913a42b298f7d617e
danilalisichkin <danilafox3467346@gmail.com>
2024-12-16 14:11:13 +0300

    frontend: add admin panel for orders

---
+++ frontend/src/pages/AdminPanel/Orders/FilterBar/FilterBar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styles from "./index.module.css";
+import arrowTop from "./icons/arrow-top.svg";
+import arrowDown from "./icons/arrow-down.svg";
+import InputSelect from "../../../../components/Inputs/InputSelect/InputSelect";
+
+function FilterBar(props) {
+  const {
+    filterFields,
+    filterField,
+    filterOrder,
+    statuses,
+    selectedStatus,
+    onSortChange,
+    onOrderChange,
+    onStatusChange,
+  } = props;
+
+  const filterOrderText =
+    filterOrder === "ASC"
+      ? `${filterField.lowerBound}-${filterField.upperBound}`
+      : `${filterField.upperBound}-${filterField.lowerBound}`;
+
+  const handleFilterFieldChange = (value) => {
+    const newSelectedField = filterFields.find(
+      (field) => field.value === value
+    );
+    if (newSelectedField) {
+      onSortChange(newSelectedField);
+    }
+  };
+
+  const handleStatusChange = (value) => {
+    const newSelectedStatus = statuses.find(
+      (status) => status.value === value
+    );
+    if (newSelectedStatus) {
+      onStatusChange(newSelectedStatus);
+    }
+  };
+
+  const toggleOrder = () => {
+    const newOrder = filterOrder === "ASC" ? "DESC" : "ASC";
+    onOrderChange(newOrder);
+  };
+
+  return (
+    <form className={styles.filterBar}>
+      <div className={styles.filterBar__filterContainer}>
+        <InputSelect
+          label="Filter by"
+          options={filterFields}
+          value={filterField.value}
+          name="filter field"
+          onChange={handleFilterFieldChange}
+        />
+      </div>
+      <div className={styles.filterBar__filterContainer}>
+        <InputSelect
+          label="Status"
+          options={statuses}
+          value={selectedStatus.value}
+          name="order status"
+          onChange={handleStatusChange}
+        />
+      </div>
+      <div className={styles.filterBar__filterContainer}>
+        <img
+          src={filterOrder === "ASC" ? arrowTop : arrowDown}
+          className={styles.filterOrder__icon}
+          alt="icon"
+          onClick={toggleOrder}
+        />
+        <p>{filterOrderText}</p>
+      </div>
+    </form>
+  );
+}
+
+export default FilterBar;
